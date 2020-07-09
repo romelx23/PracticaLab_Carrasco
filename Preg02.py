43fc2a60885a076ae756df6ad9a05e551795f9d0
AL-22 <lore.9j.lazr@gmail.com>
2020-07-09 19:49:37 +0100

    Commit 4: Ejercicio Resuelto

--- Preg02.py
+++ Preg02.py
@@ -1,1 +1,13 @@
 #Autora: Zavaleta Lorena
+
+correo=str(input("Ingrese su correro electronico institucional: "))
+validar=correo.split("@")
+if (len(validar)==2):
+    
+    if (validar[1]=="untels.edu.pe"):
+        
+        print("Correo electrónico institucional valido.")
+    
+else:
+    
+    print("Correo electrónico institucional inválido.")
