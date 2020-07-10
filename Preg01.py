37c6e35cd86bc0a9e87358d252f196895756a4b2
Michael Condori Mavila <majayesh2017@gmail.com>
2020-07-09 19:33:29 -0500

    Commit 2: Ejercicio resuelto

--- Preg01.py
+++ Preg01.py
@@ -1,3 +1,23 @@
 
 # Autor: Michael Condori Mavila
 
+def NumeroPrimo(n):
+    if n < 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        for i in range(2, n):
+            if n % i == 0:
+                return 0
+        return 1
+    
+numero=int(input("Ingrese un numero para verificar si es primo: "))
+
+if (NumeroPrimo(numero)==1):
+    
+    print(numero+" es primo.")
+    
+else:
+    
+    print(numero+" no es primo.")
