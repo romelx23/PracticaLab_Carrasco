fd1ba0fb28417db04d95289ab24ed5054fcd6744
Giannina Bernal <bri.gia.bena@gmail.com>
2020-07-09 20:29:31 -0500

    Commit 6: Ejercicio Resuelto

--- Preg03.py
+++ Preg03.py
@@ -1,1 +1,13 @@
 #Autora: Giannina Bernal Navarrete
+
+def separarvariables():
+    c1 = str(input("Ingrese la cadena: "))
+    cont = 0
+    valor = c1.split()
+    
+    for i in range (len(valor)):
+        cont = cont + len(valor[i])
+    
+    return cont
+
+print(separarvariables())
