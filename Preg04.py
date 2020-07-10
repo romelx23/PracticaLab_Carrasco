c1e4a6acf6f31e35d6a100a412f229684d502a87
Romel Alexis <romelx23@gmail.com>
2020-07-09 20:42:57 -0500

    Commit 8: Ejercicio Resuelto

--- Preg04.py
+++ Preg04.py
@@ -1,1 +1,10 @@
 #Autor:CARRASCO MALLMA ROMEL
+
+numero = int(input("Ingresa un numero: "))
+i = 0
+
+while numero > 0:
+    i = i + 1
+    resto = numero % 10
+    numero = int(numero / 10)
+    print("%d" % (resto), end = "")
